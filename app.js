cd4df7ab3fa64156cbc62ef545ea56a14fb6ec1a
Rishabh Sharma <b.rishu.pandit@gmail.com>
2022-06-27 08:59:04 +0530

    app.js updated

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0
 let score = 0
@@ -75,7 +76,7 @@ function getSelected() {
     return answer
 }
 
-aubmitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', () => {
     const answer = getSelected()
     if(answer) {
         if(answer === quizData[currentQuiz].correct) {
